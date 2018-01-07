151e79f268ad71f36f806386e1ad2fe9bf366a9b
Dane Hillard <github@danehillard.com>
2018-01-06 20:08:04 -0500

    Fix service worker registration

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,1 +1,12 @@
 import '../stylesheets/site.scss';
+
+if ('serviceWorker' in navigator) {
+    window.addEventListener('load', function(){
+        navigator.serviceWorker.register(window.serviceWorkerPath)
+        .then(function(reg) {
+            console.log('Service worker registered');
+        }).catch(function(error) {
+            console.error('Service worker not registered: ' + error);
+        });
+    });
+}
