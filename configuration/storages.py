f38b6b031ffaf108bc21a7543fb360c61343740e
Dane Hillard <github@danehillard.com>
2018-01-25 01:01:22 -0500

    Use custom domains in storages

--- configuration/storages.py
+++ configuration/storages.py
@@ -5,7 +5,9 @@ from storages.backends.s3boto3 import S3Boto3Storage
 
 class MediaStorage(S3Boto3Storage):
     bucket_name = settings.MEDIA_BUCKET_NAME
+    custom_domain = settings.MEDIA_DOMAIN
 
 
 class StaticStorage(S3Boto3Storage):
     bucket_name = settings.STATIC_BUCKET_NAME
+    custom_domain = settings.STATIC_DOMAIN
